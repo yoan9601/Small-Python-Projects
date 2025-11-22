85c20410ed83cd48f8117e16ece7f24baa400954
Yoan Boyadzhiev <yoan.boyadzhiev1@gmail.com>
2025-11-22 19:30:29 +0200

    Add word list for hangman game

---
+++ hangman/hangman_words.py
@@ -0,0 +1,6 @@
+word_list = [
+    "apple", "banana", "cherry", "dragon", "elephant", "flamingo", "giraffe", "helicopter",
+    "igloo", "jazz", "kangaroo", "lemon", "mango", "ninja", "octopus", "penguin",
+    "queen", "rainbow", "sunflower", "turtle", "umbrella", "violet", "watermelon", "xylophone",
+    "yacht", "zebra", "adventure", "beautiful", "courage", "delicious", "fantastic", "grateful"
+]
